6dd10561eee5ec239e764dc9342cbd62ce756e6b
UIT-20520649 <84429635+UIT20520649@users.noreply.github.com>
2021-06-15 13:05:59 +0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -717,3 +717,25 @@ void XoaDoAn(int x,int y)
     gotoxy(x, y);
     cout<<" ";
 }
+void XoaManHinh()
+{
+	HANDLE hOut;
+	COORD Position;
+	hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	Position.X = 0;
+	Position.Y = 0;
+	SetConsoleCursorPosition(hOut, Position);
+}
+void resizeConsole(int width, int height)
+{
+	HWND console = GetConsoleWindow();
+	RECT r;
+	GetWindowRect(console, &r);
+	MoveWindow(console, r.left, r.top, width, height, TRUE);
+}
+void textcolor(int x)
+{
+	HANDLE mau;
+	mau = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(mau, x);
+}
